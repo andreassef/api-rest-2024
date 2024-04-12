2871a4c16e84d7efe5b33ff4b323a57fbd1317a1
assef <andreassef02@gmail.com>
2024-04-12 16:23:13 -0300

    configuring supertest library

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,6 @@
-import fastify from 'fastify'
-import { env } from './env'
-import { transactionsRoutes } from './routes/transactions'
-import cookie from '@fastify/cookie'
+import { app } from "./app"
+import { env } from "./env"
 
-const app = fastify()
-
-app.register(cookie)
-app.register(transactionsRoutes, {
-  prefix: 'transactions',
-})
 
 app.listen({ port: env.PORT }).then(() => {
   console.log('HTTP Server Running!')
