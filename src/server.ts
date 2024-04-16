4891ed19c0a509d3e7bae7bdf2f6c3422f1a12f2
assef <andreassef02@gmail.com>
2024-04-16 16:03:16 -0300

    improving code

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import { app } from "./app"
 import { env } from "./env"
 
-app.listen({ port: env.PORT, host: ("RENDER"  in process.env) ? '0.0.0.0' : 'localhost' }).then(() => {
+const host = ("RENDER" in process.env) ? '0.0.0.0' : 'localhost'
+
+app.listen({ port: env.PORT, host }).then(() => {
   console.log('HTTP Server Running!')
 })
